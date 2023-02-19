ce30172e6df4c2ceeb8053ac95153f14b23197b9
유성은 <95121282+SeongSilver@users.noreply.github.com>
2023-02-19 23:05:53 +0900

    [level 0] Title: 짝수의 합, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,18 +1,5 @@
 function solution(n) {
-    let numArr = [];
-    while(n > 0){
-        console.log("aaaaa")
-        if(n%2===0){
-            numArr.push(n)
-            n -= 2;
-            console.log(numArr);
-        }else{
-            n = n-1;
-            numArr.push(n)
-            n -= 2;
-            console.log(numArr);
-        }
-    }
+    let numArr = Array(n).fill().map((_,i) => i+1).filter(i => i%2===0).reduce((a,b)=>a+b,0);
     
-    return numArr.reduce((a, b) => a+b, 0);
+    return numArr;
 }
