ccca8250ee2dc0768aa1425550b610de67dd0c41
유성은 <95121282+SeongSilver@users.noreply.github.com>
2023-02-20 20:12:48 +0900

    [level 0] Title: 개미 군단, Time: 2.38 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,8 @@
+function solution(hp) {
+    const ant1 = Math.floor(hp/5);
+    const ant2 = Math.floor((hp-ant1*5)/3);
+    const ant3 = hp-ant1*5-ant2*3;
+    console.log(ant1, ant2, ant3)    
+    
+    return ant1+ant2+ant3;
+}
