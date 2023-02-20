c37143eedcfcac8f531c0b7a01f755406c2d01ff
유성은 <95121282+SeongSilver@users.noreply.github.com>
2023-02-21 00:03:27 +0900

    [level 0] Title: 암호 해독, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,11 +1,3 @@
 function solution(cipher, code) {
-    let cipherArray = cipher.split("");
-    let answer = [];
-    console.log(cipherArray)
-    for(let i = 1; i<=cipherArray.length;i++){
-        if(i%code === 0){
-            answer.push(cipherArray[i-1]);
-        }
-    }
-    return answer.join("");
+    return cipher.split("").filter((_,i) => (i+1)%code === 0).join('');
 }
