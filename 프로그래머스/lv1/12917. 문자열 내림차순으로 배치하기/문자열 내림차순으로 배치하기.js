46f533367ea7781b3d0afae4b2defc3bd15191ae
유성은 <95121282+SeongSilver@users.noreply.github.com>
2023-02-23 23:07:11 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,21 +1,22 @@
 function solution(s) {
-    let arr1 = s.split("");
-    let arrBig = [];
-    let arrSmall = [];
-    for(let a of arr1){
-        if(a === a.toUpperCase()){
-            arrBig.push(a);
-        }else{
-            arrSmall.push(a);
-        }
-    }
+//     let arr1 = s.split("");
+//     let arrBig = [];
+//     let arrSmall = [];
+//     for(let a of arr1){
+//         if(a === a.toUpperCase()){
+//             arrBig.push(a);
+//         }else{
+//             arrSmall.push(a);
+//         }
+//     }
     
     
-    arrBig = arrBig.sort().reverse().join("");
-    arrSmall = arrSmall.sort().reverse().join("");
+//     arrBig = arrBig.sort().reverse().join("");
+//     arrSmall = arrSmall.sort().reverse().join("");
     
-    console.log(arrBig);
-    console.log(arrSmall);
+//     console.log(arrBig);
+//     console.log(arrSmall);
     
-    return arrSmall + arrBig;
+//     return arrSmall + arrBig;
+    return s.split("").sort().reverse().join("");
 }
