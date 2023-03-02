f03874997f99b2f89668f5a70f9c5f2c589a1741
유성은 <95121282+SeongSilver@users.noreply.github.com>
2023-03-02 09:51:37 +0900

    [level 0] Title: 합성수 찾기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = 0;
+    
+    for(let i = 2;i<=n;i++){
+        let count = 0;
+        for(let j = 0;j<=i;j++){
+            if(i%j===0) count++;
+        }
+        if(count>2){
+            answer++;
+        }
+    }
+    return answer;
+}
