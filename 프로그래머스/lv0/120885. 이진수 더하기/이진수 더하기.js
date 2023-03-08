3792840df38724138a1235b89c34aa66c54f8e4d
유성은 <95121282+SeongSilver@users.noreply.github.com>
2023-03-08 09:49:12 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
@@ -2,5 +2,26 @@ function solution(bin1, bin2) {
     
     return  (parseInt(bin1, 2)+parseInt(bin2, 2)).toString(2);
     
+//     let temp = Number(bin1)+Number(bin2);
+//     console.log(temp)
+//     temp = [...temp.toString()].reverse().map(v=> +v)
+//     console.log(temp)
+//     for(let i = temp.length; i<11;i++){
+//         temp.push(0);
+//     }
+//     console.log(temp)
     
+//     for(let i = 0 ; i<temp.length;i++){
+//         if(temp[i]===2){
+//             temp[i] = 0;
+//             temp[i+1]++;
+//         }else if(temp[i]===3){
+//             temp[i] = 1;
+//             temp[i+1]++;
+//         }
+//     }
+    
+        
+//     console.log(temp);
+//     console.log(Number(temp.reverse().join("")).toString());
 }
