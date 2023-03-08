45c4f0dcc3143e903072c323ca07ed1de614213e
유성은 <95121282+SeongSilver@users.noreply.github.com>
2023-03-08 14:42:01 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,12 +1,12 @@
 function solution(array) {
-    var answer = 0;
-    let strArr;
-    for(let i = 0 ;i<array.length;i++){
-        strArr = array[i].toString().split("");
-        for(let j = 0 ; j<strArr.length;j++){
-            if(strArr[j] === '7') answer++;
-        }
-    }
+    // var answer = 0;
+    // let strArr;
+    // for(let i = 0 ;i<array.length;i++){
+    //     strArr = array[i].toString().split("");
+    //     for(let j = 0 ; j<strArr.length;j++){
+    //         if(strArr[j] === '7') answer++;
+    //     }
+    // }
     
-    return answer;
+    return array.join('').split('7').length-1;
 }
