6c7475001723b4804cf43c3e6fc016ab5fb03f85
유성은 <95121282+SeongSilver@users.noreply.github.com>
2023-03-09 18:34:35 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,12 +1,14 @@
 function solution(s) {
-    let arr = s.split(" ");
-    for(let i = 0 ; i<arr.length;i++){
-        if(arr[i] === "Z"){
-            if(arr[i-1]){
-                arr.splice(i-1,2)    
-                i-=2;
-            }
-        }
-    }
-    return arr.map(a=>Number(a)).reduce((a, b) => a+b, 0);
+    // let arr = s.split(" ");
+    // for(let i = 0 ; i<arr.length;i++){
+    //     if(arr[i] === "Z"){
+    //         if(arr[i-1]){
+    //             arr.splice(i-1,2)    
+    //             i-=2;
+    //         }
+    //     }
+    // }
+    // return arr.map(a=>Number(a)).reduce((a, b) => a+b, 0);
+    
+    return s.split(' ').reduce((a, b) => b === 'Z' ? a.slice(0,-1) : [...a, b], []).map(a => Number(a)).reduce((a,b) => a+b, 0);
 }
